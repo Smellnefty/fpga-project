68f993f0ef365b24614a8ecf3f251cdc65203995
Smellnefty <nosachev.alex@gmail.com>
2020-12-26 02:58:56 +0300

    Создана операция умножения для изменения скорости движения диодов. Скорость меняется с помощью кнопки

--- ZMEI/ZMEI.sv
+++ ZMEI/ZMEI.sv
@@ -1,8 +1,8 @@
 /*
 * smellnefty
-* 17.10.2020
-* Ver. 0.2
-* Movement of diodes in both directions
+* 7.11.2020
+* Ver. 0.3
+* Speed change by button
 */
 //-------------------------------------------—
 module ZMEI (
@@ -10,6 +10,7 @@ module ZMEI (
 input logic clk,
 input logic reset,
 input logic btn1,
+input logic btn2,
 //USER
 output logic [3:0] led);
 //-------------------------------------------—
@@ -17,6 +18,15 @@ typedef enum {RIGHT, LEFT} e_state;
 e_state state;
 //-------------------------------------------—
 logic [31:0] counter;
+logic [28:0] max_cnt = 50_000_000;
+//-------------------------------------------—
+always @(negedge btn2) begin
+	case (max_cnt)
+		25_000_000: max_cnt <= 100_000_000;
+		50_000_000: max_cnt <= 25_000_000;
+		100_000_000: max_cnt <= 50_000_000;
+	endcase
+end
 //-------------------------------------------—
 always @(posedge clk) begin
 	if(!reset) begin
@@ -36,28 +46,28 @@ always @(posedge clk) begin
 				led[3] <= 1;
 				counter <= counter + 1;
 			end
-			else if(counter == 50_000_000) begin
+			else if(counter == max_cnt) begin
 				led[0] <= 1;
 				led[1] <= 0;
 				led[2] <= 1;
 				led[3] <= 1;
 				counter <= counter + 1;
 			end
-			else if(counter == 100_000_000) begin
+			else if(counter == 2 * max_cnt) begin
 				led[0] <= 1;
 				led[1] <= 1;
 				led[2] <= 0;
 				led[3] <= 1;
 				counter <= counter + 1;
 			end
-			else if(counter == 150_000_000) begin
+			else if(counter == 3 * max_cnt) begin
 				led[0] <= 1;
 				led[1] <= 1;
 				led[2] <= 1;
 				led[3] <= 0;
 				counter <= counter + 1;
 			end
-			else if(counter == 200_000_000) begin
+			else if(counter == 4 * max_cnt) begin
 				counter <= 0;
 			end
 			else begin
@@ -83,28 +93,28 @@ always @(posedge clk) begin
 				led[3] <= 0;
 				counter <= counter + 1;
 			end
-			else if(counter == 50_000_000) begin
+			else if(counter == max_cnt) begin
 				led[0] <= 1;
 				led[1] <= 1;
 				led[2] <= 0;
 				led[3] <= 1;
 				counter <= counter + 1;
 			end
-			else if(counter == 100_000_000) begin
+			else if(counter == 2 * max_cnt) begin
 				led[0] <= 1;
 				led[1] <= 0;
 				led[2] <= 1;
 				led[3] <= 1;
 				counter <= counter + 1;
 			end
-			else if(counter == 150_000_000) begin
+			else if(counter == 3 * max_cnt) begin
 				led[0] <= 0;
 				led[1] <= 1;
 				led[2] <= 1;
 				led[3] <= 1;
 				counter <= counter + 1;
 			end
-			else if(counter == 200_000_000) begin
+			else if(counter == 4 * max_cnt) begin
 				counter <= 0;
 			end
 			else begin
