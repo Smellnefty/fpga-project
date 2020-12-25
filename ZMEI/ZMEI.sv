b0c24e7a4d1d387c00b63d47411f4d9f804352b2
Smellnefty <nosachev.alex@gmail.com>
2020-12-26 02:25:59 +0300

    Добавлено движение диодов влево. Сделано переключение направлений с помощью кнопки

--- ZMEI/ZMEI.sv
+++ ZMEI/ZMEI.sv
@@ -1,18 +1,19 @@
 /*
 * smellnefty
-* 10.10.2020
-* Ver. 0.1
-* Diodes move to the right
+* 17.10.2020
+* Ver. 0.2
+* Movement of diodes in both directions
 */
 //-------------------------------------------—
 module ZMEI (
 //SYSTEM
 input logic clk,
 input logic reset,
+input logic btn1,
 //USER
 output logic [3:0] led);
 //-------------------------------------------—
-typedef enum {RIGHT} e_state;
+typedef enum {RIGHT, LEFT} e_state;
 e_state state;
 //-------------------------------------------—
 logic [31:0] counter;
@@ -60,7 +61,64 @@ always @(posedge clk) begin
 				counter <= 0;
 			end
 			else begin
+				if(!btn1) begin
+					led[0] <= 0;
+					led[1] <= 0;
+					led[2] <= 0;
+					led[3] <= 0;
+					counter <= 0;
+					state <= LEFT;
+				end
+				else begin
+					counter <= counter + 1;
+				end
+			end
+		end
+	else 
+		if(state == LEFT) begin
+			if(counter == 0) begin
+				led[0] <= 1;
+				led[1] <= 1;
+				led[2] <= 1;
+				led[3] <= 0;
 				counter <= counter + 1;
+			end
+			else if(counter == 50_000_000) begin
+				led[0] <= 1;
+				led[1] <= 1;
+				led[2] <= 0;
+				led[3] <= 1;
+				counter <= counter + 1;
+			end
+			else if(counter == 100_000_000) begin
+				led[0] <= 1;
+				led[1] <= 0;
+				led[2] <= 1;
+				led[3] <= 1;
+				counter <= counter + 1;
+			end
+			else if(counter == 150_000_000) begin
+				led[0] <= 0;
+				led[1] <= 1;
+				led[2] <= 1;
+				led[3] <= 1;
+				counter <= counter + 1;
+			end
+			else if(counter == 200_000_000) begin
+				counter <= 0;
+			end
+			else begin
+				if(!btn1) begin
+					led[0] <= 0;
+					led[1] <= 0;
+					led[2] <= 0;
+					led[3] <= 0;
+					counter <= 0;
+					state <= RIGHT;
+				end
+				else begin
+					counter <= counter + 1;
+				end
 			end
 		end
 end
